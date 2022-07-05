5976a1dd37f5f929946ed470aee6680aceb53f1c
tobiasgmueller <tgmueller@ucdavis.edu>
2022-07-05 13:30:04 -0400

    added graphs and analysis

--- asco_2022_analysis_tm.R
+++ asco_2022_analysis_tm.R
@@ -4,32 +4,86 @@
 
 
 # first a few packages
-library(tidyverse)
+library(tidyverse) # for everything
+library(rstatix) # for pipe friendly stats
+library(lme4) # for models
+
+library(survival)
+library(survminer)
+library(broom)
+
 rm(list=ls()) # then clean the environment
 
 
 # then read in the data
 summary_df <- read_csv("input/asco_plate_maps_summary.csv")
 
+summary_df$end_status <- as.factor(summary_df$end_status)
+summary_df$plate <- as.factor(summary_df$plate)
+summary_df$well <- as.factor(summary_df$well)
+summary_df$larva_nest <- as.factor(summary_df$larva_nest)
+summary_df$larva_number <- as.factor(summary_df$larva_number)
+summary_df$treatment <- as.factor(summary_df$treatment)
+summary_df$larva_stage <- as.factor(summary_df$larva_stage)
+
+
+df<- summary_df%>%
+  filter(end_status != "removed_dead")
 
 # then a table perchance?
 # a little graffy waffy?
 
 
-summary_df %>%
+df %>%
   ggplot()+
-  geom_bar(aes(x=status, fill=treatment))
+  geom_bar(aes(x=end_status, fill=treatment))
 
 # and then just looking at the dead ones
-summary_df %>%
+df %>%
   filter(status=="dead")%>%
   ggplot()+
-  geom_bar(aes(x=status, fill=treatment))
+  geom_bar(aes(x=end_status, fill=treatment))
+
+
+def_to_cocoon<- df %>%
+  ggplot()+
+  geom_boxplot(aes(x = treatment, y=def_to_cocoon, fill=treatment))+
+  geom_point(aes(x = treatment, y=def_to_cocoon, fill=treatment))
+
+ggsave(plot= def_to_cocoon, "output/def_to_cocoon.jpeg")
+
+df %>%
+  kruskal_test(def_to_cocoon~treatment)
+
+df %>%
+  dunn_test(def_to_cocoon~treatment)
 
 
 
 
 
+def_to_cocoon_model <- lm(data=df, def_to_cocoon~treatment)
+summary(def_to_cocoon_model)                   
+                             
+write.csv(tidy(def_to_cocoon_model), "output/def_to_cocoon_model.csv")
+
+
+
+death_model <- glmer(data=df, status~treatment + (1|plate), family=binomial,
+                     glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 100000)))
+
+
+
+summary(glm(data=df, end_status~treatment, family=binomial))
+
+
+# test of a cox model
+
+
+a.cox <- coxph(Surv(time_to_event, status)~treatment, data=df)
+
+
+ggsurvplot(surv_fit(a.cox), color="#2E9FDF", ggtheme = theme_minimal())
 
 
 
